95372ec53909089a5d9358bfad0031fff424f0e9
dhayithri <dhayithrim@gmail.com>
2016-12-05 12:01:31 +0530

    collaboration frontend

---
+++ WebContent/Friend/myfriendsctrl.js
@@ -0,0 +1,7 @@
+app.controller('myfriendctrl',['$scope','$http',function($scope,$http){
+	var BASE_URL = 'http://localhost:8081/Backend';
+	
+	$scope.getmyfriends=function(){
+		
+	}
+}])
---
+++ WebContent/Users/usersctrl.js
@@ -0,0 +1,33 @@
+app.controller('userctrl', [ '$scope', '$http', function($scope, $http) {
+	var BASE_URL = 'http://localhost:8081/Backend';
+	$scope.submit = function() {
+		
+		$scope.users = {	
+			
+			username : $scope.username,
+			mail:$scope.mail,
+			password:$scope.password,
+			mobile : $scope.mobile,
+			dob:$scope.dob,
+			role:$scope.role,
+			address:$scope.address,
+			gender:$scope.gender
+		}
+		$http({
+			method : 'POST',
+			url : BASE_URL + '/register',
+			data : $scope.users
+		}).success(function(data, status, headers, config) {
+			$scope.username='';
+			$scope.mail='';
+			$scope.password='';
+			$scope.mobile='';
+			$scope.dob='';
+			$scope.role='';
+			$scope.address='';
+			$scope.gender='';
+		}).error(function(data,status,headers,config){
+			alert("error");
+		});
+	};
+}]);
