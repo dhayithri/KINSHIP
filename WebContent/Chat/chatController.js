c9866cc293c42a1ea3313428a71fdf7f91334510
dhayithri <dhayithrim@gmail.com>
2016-12-15 17:50:01 +0530

    collaboration frontend

--- WebContent/Chat/chatController.js
+++ WebContent/Chat/chatController.js
@@ -1,16 +1,16 @@
 console.log("app")
-app.controller("chatController",function($scope,ChatService){
+app.controller("chatController", function($scope, ChatService) {
 	console.log("chatctrl")
-	$scope.messages=[];
-	$scope.message="";
-	$scope.max=140;
-	
-	$scope.addMessage=function(){
+	$scope.messages = [];
+	$scope.message = "";
+	$scope.max = 140;
+
+	$scope.addMessage = function() {
 		console.log("chat")
 		ChatService.send($scope.message);
-		$scope.message="";
+		$scope.message = "";
 	};
-	ChatService.receive().then(null,null,function(message){
+	ChatService.receive().then(null, null, function(message) {
 		$scope.messages.push(message);
-		})
+	})
 })
